e293570426ad3e9473909f7480b74d7b4e9ffa53
mpaszkowski <paszkowski.mikolaj98@gmail.com>
2025-11-08 13:32:40 +0100

    remove deprecated files and modules, including data processing notebook, training scripts, and UI components, to streamline the codebase

--- src/recommendation_engine/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file is intentionally left empty to make the directory a Python package. 
-
-# Empty file to make recommendation_engine a package 
--- src/recommendation_engine/recommenders/__init__.py
+++
@@ -1,5 +0,0 @@
-from .collab_item_based_knn_rec import CollabItemBasedKNNRec
-from .content_hybrid_item_based_rec import ContentHybridItemBasedRec
-from ..utils.abstract_recommender_data_loader import AbstractRecommenderDataLoader
-
-__all__ = ['CollabItemBasedKNNRec', 'ContentHybridItemBasedRec', 'AbstractRecommenderDataLoader']
